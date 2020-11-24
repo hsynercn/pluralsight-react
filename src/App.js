077abe6841ba5363b4ce3ad9f362f05d00bc0a02
hsynercn <huscanercan@gmail.com>
2020-11-24 21:54:23 +0300

    First hook sample

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello React!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <Hello/>
+                <Button/>
+                <p>Hello React!</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
-function Hello(){
-  return <div>Hello React!</div>
+function Hello() {
+    return <div>Hello React func!</div>
+}
+
+function Button() {
+    //const [currentStateValue, functionToSetNewStateValue] = useState(initialStateValue)
+    const [counter, setCounter] = useState(5)
+    return <button onClick={() => {
+        setCounter(counter * 2)
+    }}>{counter}</button>;
+}
+
+function increment() {
+    console.log("CLicked!");
 }
 
 export default App;

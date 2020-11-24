22270d435c4b2f050d92616a8820371899945a84
hsynercn <huscanercan@gmail.com>
2020-11-24 20:35:15 +0300

    Initial element commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hello React!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +21,10 @@ function App() {
       </header>
     </div>
   );
+}
+
+function Hello(){
+  return <div>Hello React!</div>
 }
 
 export default App;

75017ccef1379f52dfad0efb7f0ceb82366d4fcf
hsynercn <huscanercan@gmail.com>
2020-11-25 23:24:18 +0300

    The basics completed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <Hello/>
-                <Unified/>
                 <p>Hello React!</p>
                 <a
                     className="App-link"
@@ -28,35 +27,6 @@ function App() {
 
 function Hello() {
     return <div>Hello func!</div>
-}
-
-function Button(props) {
-    const handleClick = () => props.onClickFunction(props.increment);
-    return (
-        <button onClick={handleClick}>
-            +{props.increment}
-        </button>
-    );
-}
-
-function Display(props) {
-    return (
-        <div>
-            {props.message}
-        </div>
-    );
-}
-
-function Unified() {
-    const [counter, setCounter] = useState(55);
-    const incrementCounter = (incrementValue) => setCounter(counter + incrementValue);
-    return (<div>
-        <Button onClickFunction={incrementCounter} increment={1}/>
-        <Button onClickFunction={incrementCounter} increment={5}/>
-        <Button onClickFunction={incrementCounter} increment={10}/>
-        <Button onClickFunction={incrementCounter} increment={100}/>
-        <Display message={counter}/>
-    </div>);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Unified from "./MyFirstElement";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div><App /><Unified /></div>
   </React.StrictMode>,
   document.getElementById('root')
 );

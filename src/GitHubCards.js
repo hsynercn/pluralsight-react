9783ba1fab54e72ad745900fc5046c26eef665b6
hsynercn <huscanercan@gmail.com>
2020-12-01 13:12:46 +0300

    React ref example

--- src/GitHubCards.js
+++ src/GitHubCards.js
@@ -1,14 +1,44 @@
 import React from "react";
-import App from "./App";
+import './GitHubCards.css';
+
+
+const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+const CardList = (props) => (
+    <div>
+        {props.profiles.map(profile => <Card {...profile}/>)}
+    </div>
+);
+
+class Form extends React.Component {
+    userNameInput = React.createRef();
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.userNameInput.current.value)
+    };
+    render() {
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="GitHub username" ref={this.userNameInput} required/>
+                <button>Add card</button>
+            </form>
+        );
+    }
+}
 
 class Card extends React.Component {
     render() {
+        const profile = this.props;
         return (
             <div className="github-profile">
-                <img src="https://placehold.it/75" />
+                <img src={profile.avatar_url}/>
                 <div className="info">
-                    <div className="name">Name here...</div>
-                    <div className="company">Company here...</div>
+                    <div className="name">{profile.name}</div>
+                    <div className="company">{profile.company}</div>
                 </div>
             </div>
         );
@@ -16,8 +46,23 @@ class Card extends React.Component {
 }
 
 class Container extends React.Component {
+    /*constructor(props) {
+        super(props);
+        this.state = {
+            profiles: testData
+        };
+    }*/
+
+    state = {
+        profiles: testData
+    };
     render() {
-        return(<div><Card /></div>);
+        return (
+            <div>
+                <Form/>
+                <CardList profiles={this.state.profiles}/>
+            </div>
+        );
     }
 }
 
